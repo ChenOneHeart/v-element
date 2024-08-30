a99bbec4bcdb3f142ed167502345a1b1c5c0dcbc
chenyiheng <chenoneheat@github.com>
2024-08-30 14:30:30 +0800

    feat: Button组件封装

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,40 @@
+<template>
+  <button
+    ref="_ref"
+    class="vk-button"
+    :class="{
+      [`vk-button--${type}`]: type,
+      [`vk-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+    }"
+    :disabled="disabled"
+    :autofocus="autofocus"
+    :type="nativeType"
+  >
+    <span><slot></slot></span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  name: "VKButton",
+});
+</script>
+<script setup lang="ts">
+import type { ButtonProps } from "./types";
+withDefaults(defineProps<ButtonProps>(), {
+  nativeType: "button",
+});
+const _ref = ref<HTMLButtonElement>();
+defineExpose({
+  ref: _ref,
+});
+</script>
+
+<style scoped>
+
+</style>
